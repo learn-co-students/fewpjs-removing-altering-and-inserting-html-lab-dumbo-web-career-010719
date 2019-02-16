b0384bd4b7f7d689c76ff50ce4540c63c798985d
woodylucas <wlucas0311@gmail.com>
2019-02-16 11:58:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('#main').remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerText = "WOODY is the champion"
