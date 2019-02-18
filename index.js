594a0e29c8956f220a5f6572baacc39d030d1de4
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-17 23:18:19 -0500

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerText = "Flatiron School is the champion!";
+newHeader.innerText = "Lizzy is the champion!";
 newHeader.className = "victory";
