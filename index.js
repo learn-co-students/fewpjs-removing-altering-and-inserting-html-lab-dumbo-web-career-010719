32458a818144bfbf147d3daa86e3c5062f2d42f7
Redmond Chan <redmondchan92@gmail.com>
2019-02-19 09:54:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.getElementById('main').remove()
+
+var newHeader = document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerText = "YOUR-NAME is the champion";
+document.body.appendChild(newheader);
